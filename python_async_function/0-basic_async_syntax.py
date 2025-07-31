d0920ceb6f865c330ff847fe53168f96a01f127a
HB10065 <10065@holbertonstudents.com>
2025-07-31 23:25:07 +0000

    a

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,10 +5,10 @@ Module Doc
 import random, asyncio
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     '''
     Function Doc
     '''
-    num = random.random(0.0, max_delay)
+    num = random.random(0.0, float(max_delay))
     await asyncio.sleep(num)
     return num
