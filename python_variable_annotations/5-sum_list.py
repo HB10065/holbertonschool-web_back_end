5e7422afd6acb06aca60c0f9c23d79f757fdd322
HB10065 <10065@holbertonstudents.com>
2025-07-31 18:56:38 +0000

    5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+Module Doc
+'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''
+    Function Doc
+    '''
+    sum: float = 0.00
+    for element in input_list:
+        sum += element
+
+    return sum
