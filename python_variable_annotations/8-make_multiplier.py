201dcb6d8796ec13b3ba228086ccfdc9135a55b2
HB10065 <10065@holbertonstudents.com>
2025-07-31 19:50:53 +0000

    8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Module Doc
+'''
+import typing
+
+
+def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    '''
+    Function Doc
+    '''
+    def multi(n: float):
+        '''
+        Function doc
+        '''
+        return n * multiplier
+    
+    return multi
