dc5f5d504b34588d1ee4ae6d36370b3e0ba85a81
HB10065 <10065@holbertonstudents.com>
2025-08-01 00:21:12 +0000

    1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+Module Doc
+'''
+import random
+import asyncio
+import importlib
+import typing
+
+
+wait_random = wait_random = importlib.import_module('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
+    '''
+    Function Doc
+    '''
+    wait_list = []
+    task = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+
+    for a in asyncio.as_completed(task):
+        i = await a
+        wait_list.append(i)
+
+    return wait_list
