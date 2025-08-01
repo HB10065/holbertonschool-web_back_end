49efdd3324c0b128fadf9b289723afc85e4b5d51
HB10065 <10065@holbertonstudents.com>
2025-08-01 01:36:23 +0000

    2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+Module Doc
+'''
+import asyncio
+import time
+import importlib
+async_comprehension = importlib.import_module('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''
+    Function Doc
+    '''
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension())
+    end = time.perf_counter()
+
+    return end - start
