0bf496a071ad4fe810c0c685ea3b09f5271d49c8
HB10065 <10065@holbertonstudents.com>
2025-08-01 01:10:41 +0000

    await

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -12,5 +12,5 @@ async def async_generator() -> typing.Generator[float, None, None]:
     Function Doc
     '''
     for _ in range(10):
-        asyncio.sleep(1)
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
