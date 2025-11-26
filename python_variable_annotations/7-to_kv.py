136e6e2512ed558b6df98d0d89156c9d62d6a22b
HB10065 <10065@holbertonstudens.com>
2025-11-26 08:38:02 -0800

    doc

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,4 +4,5 @@ import typing
 
 
 def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    '''returns tuple of the given string and the square of the given number'''
     return (k, v ** 2.0)
