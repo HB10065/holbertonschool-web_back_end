5e068e7838d7ded6f28d6c48e016b8f5b4b8ef85
HB10065 <10065@holbertonstudens.com>
2025-11-26 08:25:32 -0800

    6: fc

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Module Doc'''
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.List[int | float]) -> float:
+    '''
+    Takes a list of floats and ints and returns their sum
+    '''
+    list_sum: float = 0.0
+    for num in mxd_lst:
+        list_sum = list_sum + num
+
+    return list_sum
