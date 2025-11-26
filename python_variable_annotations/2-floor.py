1d23377a13e88ba213081632290ad314f7dbea8c
HB10065 <10065@holbertonstudens.com>
2025-11-26 07:57:34 -0800

    2: floor of a number using round()

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''Module Doc'''
+
+
+def floor(n: float) -> int:
+    '''
+    a function that takes a number and
+    returns the floor of that number
+    '''
+    return round(n)
