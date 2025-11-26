ad8cbb7147db8dc1bc4880373cc228b1611a4d3b
HB10065 <10065@holbertonstudens.com>
2025-11-26 08:37:04 -0800

    q

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 '''Module Doc'''
 import typing
-import math
 
 
 def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
-    sq: float = math.sqrt(v)
-    return (k, sq)
+    return (k, v ** 2.0)
