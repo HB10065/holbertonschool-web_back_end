06ee792fe130c9f0a79edc1a13ee2bd8e1666b00
HB10065 <10065@holbertonstudens.com>
2025-11-26 08:50:51 -0800

    8: inner

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,8 @@ import typing
 
 
 def make_multiplier(multiplier: float) -> typing.Callable[[float], float]:
+    '''takes a number and returns a function that multiplies a 
+    number by the given number'''
     def inner(x: float) -> float:
         return x * multiplier
     return inner
