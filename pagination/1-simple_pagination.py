3fe86a344c04179884b349c41f7060e7be5db78e
HB10065 <10065@holbertonstudents.com>
2026-02-12 08:47:17 -0800

    task1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -39,6 +39,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''documentation
+        '''
         assert (type(page) == int and type(page_size) == int)
         assert (page > 0 and page_size > 0)
         start, end = index_range(page, page_size)
