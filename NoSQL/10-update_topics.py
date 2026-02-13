1ecc1ca7fde320f5c15c99933af423a49b550e80
HB10065 <10065@holbertonstudents.com>
2026-02-13 14:56:25 -0800

    task10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -5,8 +5,8 @@
 def update_topics(mongo_collection, name, topics):
     '''Update
     '''
-    update  =  mongo_collection.update_many(
-        {'name': name},
-        {'$set': {'topics': topics}}
-    )
+    update = mongo_collection.update_many(
+            {"name": name},
+            {"$set": {"topics": topics}}
+        )
     return update
