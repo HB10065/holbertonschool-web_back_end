9bee374059bfd47c797a3b53968f21a2b0fd8fcc
HB10065 <10065@holbertonstudents.com>
2026-02-13 15:23:02 -0800

    task1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -2,5 +2,9 @@ import ClassRoom from "./0-classroom";
 
 
 export default function intializeRooms() {
-    return [ClassRoom(19), ClassRoom(20), ClassRoom(34)]
+    return [
+        new ClassRoom(19),
+        new ClassRoom(20),
+        new ClassRoom(34)
+    ];
 }
