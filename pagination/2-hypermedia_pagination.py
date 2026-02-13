2932cbcb89c096c9289974b8433dcb0c754d0338
HB10065 <10065@holbertonstudents.com>
2026-02-13 09:49:48 -0800

    task2

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+'''
+Docstring for pagination.0-simple_helper_function
+'''
+import csv
+from typing import List
+import math
+
+
+def index_range(page, page_size):
+    '''
+    Docstring for index_range
+    '''
+    if page < 1:
+        raise ValueError('page must be greater than 0')
+    if page_size < 1:
+        raise ValueError('page_size must be greater than 0')
+
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''documentation
+        '''
+        assert (isinstance(page, int) and isinstance(page_size, int))
+        assert (page > 0 and page_size > 0)
+        start, end = index_range(page, page_size)
+        if len(self.dataset()) < end:
+            return []
+        return self.__dataset[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10):
+        '''Documentation
+        '''
+        if page == 1:
+            PrevP = None
+        else:
+            PrevP = page - 1
+
+        if self.get_page(page + 1, page_size) == []:
+            NextP = None
+        else:
+            NextP = page + 1
+
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': NextP,
+            'prev_page': PrevP,
+            'total_pages': math.ceil(len(self.dataset()) / page_size)
+        }
