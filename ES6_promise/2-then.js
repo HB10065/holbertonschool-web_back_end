03188695abb311ec3cec232650d528856ef0814b
HB10065 <10065@holbertonstudents.com>
2026-02-14 13:59:55 -0800

    task2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -5,7 +5,7 @@ export default function handleResponseFromAPI(promise) {
             body: 'success'
     }))
     
-    .catch(() => {new Error()})
+    .catch(() => new Error())
     
     .finally(() => {
         console.log('Got a response from the API')
