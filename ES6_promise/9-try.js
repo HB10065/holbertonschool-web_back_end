9208be81f329e4071307ed479a1e61ce9d26cb74
HB10065 <10065@holbertonstudents.com>
2026-02-14 15:44:46 -0800

    task9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
         queue.push(mathFunction());
     }
     catch (error) {
-        queue.push(error);
+        queue.push(error.message);
     }
     finally {
         queue.push('Guardrail was processed');
