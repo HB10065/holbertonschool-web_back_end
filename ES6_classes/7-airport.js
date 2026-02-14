a5df026b9dfd8e97d0d94359bc3698a0a80fe93e
HB10065 <10065@holbertonstudents.com>
2026-02-14 07:02:00 -0800

    git push

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -5,7 +5,7 @@ export default class Airport {
     }
 
     // methods
-    toString() {
-        return `[object ${this._code}]`
+    get [Symbol.toStringTag]() {
+        return this._code
     }
 }
