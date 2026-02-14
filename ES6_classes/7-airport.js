425feb2a3ab36984f78d074e44f4df3b9a4e500c
HB10065 <10065@holbertonstudents.com>
2026-02-14 06:53:24 -0800

    task7

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+export default class Airport {
+    constructor(name, code) {
+        this._name = name;
+        this._code = code;
+    }
+
+    // methods
+    toString() {
+        return `Airport ${this._code}`
+    }
+}
