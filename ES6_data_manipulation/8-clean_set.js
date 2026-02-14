a411e76b26c0fdd40f82eb772083d41c3f9acd4e
HB10065 <10065@holbertonstudents.com>
2026-02-14 11:54:38 -0800

    task8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, startString) {
+    if (typeof startString !== 'string') {
+        return ''
+    }
+    
     newString = ''
     for (const element of [...set]) {
         if (element.startsWith(startString)) {
